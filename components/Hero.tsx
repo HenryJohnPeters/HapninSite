c3572eb19e8e9ed63913cf2954bf31711392f540
Henry Peters <hpeters@metawin.inc>
2023-05-04 19:57:50 +0100

    wip

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,8 +8,8 @@ import barbecue from "../public/static/barbecue-party.svg";
 const Hero = () => {
   return (
     <div className="w-full sm:h-screen flex flex-col justify-between px-6 sm:px-10">
-      <div className="mx-auto sm:mx-4 md:mx-12 mt-28 mb-8 sm:my-auto xl:mx-[6rem] 2xl:w-[1200px] 2xl:mx-auto">
-        {/* TOP */}
+      {/* <div className="mx-auto sm:mx-4 md:mx-12 mt-28 mb-8 sm:my-auto xl:mx-[6rem] 2xl:w-[1200px] 2xl:mx-auto">
+
         <div className="w-full sm:mb-9 sm:flex sm:items-center">
           <h1 className="font-bold text-2xl sm:mr-6 mb-4">
             The platform - where people are brought together
@@ -21,37 +21,70 @@ const Hero = () => {
             cost events in your area.
           </p>
           <Image src={barbecue} alt="party" className="hidden sm:block" />
+        </div> */}
+
+{/*
+              <Parallax bgImage={charity} strength={500}>
+        <div className="mx-auto sm:mx-4 md:mx-12 mt-28 mb-8 sm:my-auto xl:mx-[6rem] 2xl:w-[1200px] 2xl:mx-auto">
+          <animated.h1
+            className="font-bold text-2xl sm:mr-6 mb-4"
+            style={titleAnimation}
+          >
+            <Hover options={titleHoverOptions}>
+              The platform - where people are brought together
+            </Hover>
+          </animated.h1>
+          <p
+            className=""
+            style={{ boxShadow: "0 5px 15px rgba(0,0,0,.3)", border: "2px solid #70f1ae" }}
+          >
+            At <span className="font-bold">HAPNIN</span>, we believe that local
+            events and activities are the heart of any community. That&apos;s
+            why we created our app to help you discover the best free and low
+            cost events in your area.
+          </p>
+          <Hover options={imageHoverOptions}>
+            <Image src={barbecue} alt="party" className="hidden sm:block" />
+          </Hover>
         </div>
+      </Parallax> */}
+
 
         {/* BOTTOM */}
         <div className="w-full relative">
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-4">
             {/* CARD 1 */}
-            <div className="w-full h-[160px] mb-12">
+            <div className="w-full h-[160px] mb-12 overflow-hidden rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-all">
               <Image
                 src={neighbours}
                 alt="charity"
                 className="h-[190px] object-cover hover:border-[#70f1ae] hover:border-2"
               />
-              <p>Connect with your neighbours</p>
+              <p className="absolute bottom-0 w-full py-2 text-center font-bold bg-black bg-opacity-60 text-white">
+                Connect with your neighbours
+              </p>
             </div>
             {/* CARD 2 */}
-            <div className="w-full h-[160px] mb-12">
+            <div className="w-full h-[160px] mb-12 overflow-hidden rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-all">
               <Image
                 src={charity}
                 alt="charity"
                 className="h-[190px] object-cover hover:border-[#70f1ae] hover:border-2"
               />
-              <p>Support your local charity</p>
+              <p className="absolute bottom-0 w-full py-2 text-center font-bold bg-black bg-opacity-60 text-white">
+                Support your local charity
+              </p>
             </div>
             {/* CARD 3 */}
-            <div className="w-full h-[160px] mb-12">
+            <div className="w-full h-[160px] mb-12 overflow-hidden rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-all">
               <Image
                 src={event}
                 alt="charity"
                 className="h-[190px] object-cover hover:border-[#70f1ae] hover:border-2"
               />
-              <p>Create your own event</p>
+              <p className="absolute bottom-0 w-full py-2 text-center font-bold bg-black bg-opacity-60 text-white">
+                Create your own event
+              </p>
             </div>
           </div>
         </div>
---
+++ components/card/card.tsx
@@ -0,0 +1,22 @@
+// Card component
+
+// interface Props {
+//     name?: string;
+//     image: string;
+//   }
+
+//   function Card({ name, image }: Props) {
+//     return (
+//       <div className="cursor-pointer rounded-md overflow-hidden shadow-md mb-5 min-w-lg">
+//         <div className="flex flex-col h-full">
+//           <div className="relative flex-grow-1">
+//             <Image src={image} alt="Coffee" layout="fill" objectFit="cover" />
+//           </div>
+//           <div className="px-4 py-4">
+//             <div className="font-medium mb-1">{name || "Coffee"}</div>
+//             <p className="text-gray-700 text-sm">PHP 150.00</p>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
