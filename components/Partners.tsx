13704b148e5238126031cba3fd2194cf586cd657
Joan Gerard <joan.gerard@outlook.com>
2023-05-04 17:48:21 +0200

    test

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -9,9 +9,7 @@ const Partners = () => {
         <div className="mb-4">
           <h2 className="font-bold text-2xl">Our Partners</h2>
         </div>
-
         <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-
         </div>
       </div>
     </div>
