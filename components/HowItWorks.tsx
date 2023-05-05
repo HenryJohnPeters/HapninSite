e872ba7e9588457b2ca65f5b85a402a09b6775a3
Joan Gerard <joan.gerard@outlook.com>
2023-05-05 19:59:54 +0200

    improve responsiveness for breakpoints 640px onwards

--- components/HowItWorks.tsx
+++ components/HowItWorks.tsx
@@ -16,9 +16,9 @@ interface Props {
 
 function Card({ image, name, description }: Props) {
   return (
-    <div className="cursor-pointer rounded-md overflow-hidden shadow-md mb-5 min-w-lg flex flex-row hover:bg-green-50 transition-colors duration-300 bg-blue-400">
+    <div className="cursor-pointer rounded-md overflow-hidden shadow-md mb-5 min-w-lg flex flex-row items-center hover:bg-green-50 transition-colors duration-300 md:4-[400px] lg:w-[400px] lg:h-[400px]">
       {/* IMAGE */}
-      <div className="relative w-1/3 sm:w-1/4 h-full overflow-hidden bg-black rounded-xl p-1">
+      <div className="relative w-1/3 sm:w-1/4 md:w-1/3 lg:w-1/2 h-full overflow-hidden bg-black rounded-xl p-1">
         {/* <div className="absolute inset-0 bg-green-50 transform -rotate-6 origin-top-left"></div>
         <div className="absolute inset-0 bg-green-50 transform rotate-6 origin-top-right"></div> */}
         {/* <div className="absolute inset-0 flex items-center justify-center">
@@ -33,11 +33,13 @@ function Card({ image, name, description }: Props) {
       </div>
 
       {/* TEXT */}
-      <div className="p-6 flex-shrink-0 w-2/3 sm:w-3/4 overflow-hidden bg-red-400">
+      <div className="p-6 flex-shrink-0 w-2/3 md:w-2/3 lg:w-1/2 overflow-hidden">
         <div className="font-medium mb-1 text-lg text-gray-800 hover:text-green-500 transition-colors duration-300">
           {name || "Coffee"}
         </div>
-        <p className="text-gray-700 text-2 overflow-hidden">{description}</p>
+        <p className="text-gray-700 text-2 md:text-base xl:text-sm overflow-hidden">
+          {description}
+        </p>
         {/* <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 mt-3 rounded-md transition-colors duration-300">
           Learn More
         </button> */}
@@ -54,8 +56,8 @@ const HowItWorks = () => {
     <>
       <Element name="howItWorks" />
       <div className="w-full px-6 sm:px-10 my-[6rem]">
-        <div className="xl:mx-[6rem] 2xl:w-[1200px] 2xl:mx-auto">
-          <div className="mb-4">
+        <div className="xl:mx-[6rem] 2xl:w-[1400px] 2xl:mx-auto">
+          <div className="mb-10">
             <h2 className="font-bold text-2xl text-center mb-2">
               How HAPNIN works
             </h2>
@@ -63,11 +65,12 @@ const HowItWorks = () => {
           </div>
 
           <Swiper
-            spaceBetween={100}
+            // spaceBetween={100}
             breakpoints={{
               640: { slidesPerView: 1 },
-              768: { slidesPerView: 2 },
-              1024: { slidesPerView: 3 },
+              768: { slidesPerView: 2, spaceBetween: 40 },
+              1024: { slidesPerView: 2, spaceBetween: 10 },
+              1280: { slidesPerView: 3, spaceBetween: 40 },
             }}
             pagination={{
               clickable: true,
@@ -102,11 +105,6 @@ const HowItWorks = () => {
               />
             </SwiperSlide>
           </Swiper>
-          {/* <div className="swiper-pagination mt-4 flex justify-center items-center">
-      <span className="swiper-pagination-bullet inline-block w-3 h-3 rounded-full bg-white mr-2"></span>
-      <span className="swiper-pagination-bullet inline-block w-3 h-3 rounded-full bg-white mr-2"></span>
-      <span className="swiper-pagination-bullet inline-block w-3 h-3 rounded-full bg-white"></span>
-    </div> */}
         </div>
       </div>
     </>
