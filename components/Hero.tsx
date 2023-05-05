c23b55b039a418481165ba3f21269e4cd22affc7
Joan Gerard <joan.gerard@outlook.com>
2023-05-05 14:47:35 +0200

    added missing Element targets for react scroll

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Image from "next/image";
+import { Element } from "react-scroll";
+
 import charity from "../public/static/charity.jpg";
 import event from "../public/static/event.jpg";
 import neighbours from "../public/static/neighbours.jpg";
@@ -7,35 +9,40 @@ import barbecue from "../public/static/barbecue-party.svg";
 
 const Hero = () => {
   return (
-    <div className="bg-gradient-to-br from-green-500 to-blue-500 w-full min-h-screen flex flex-col justify-center items-center py-10 px-6 sm:px-10 lg:px-20">
-      <div className="mx-auto sm:mx-4 md:mx-12 lg:mx-0 lg:w-1/2 xl:w-[800px] 2xl:w-[1200px]">
-        <div className="flex flex-col sm:flex-row justify-between items-center sm:py-12">
-          <div className="flex flex-col justify-center sm:w-1/2">
-            <h1 className="font-bold text-3xl lg:text-4xl text-white mb-6 leading-tight">
-              The platform - where people are brought together
-            </h1>
-            <p className="text-lg text-white mb-8">
-              At <span className="font-bold">HAPNIN</span>, we believe that local events and activities are the heart of any community. That&apos;s why we created our app to help you discover the best free and low cost events in your area.
-            </p>
-            <button className="bg-white text-purple-500 hover:text-white hover:bg-purple-500 rounded-full py-3 px-6 font-bold shadow-lg transition-all duration-200">
-              Get Started
-            </button>
+    <>
+      <Element name="hero" />
+      <div className="bg-gradient-to-br from-green-500 to-blue-500 w-full min-h-screen flex flex-col justify-center items-center py-10 px-6 sm:px-10 lg:px-20">
+        <div className="mx-auto sm:mx-4 md:mx-12 lg:mx-0 lg:w-1/2 xl:w-[800px] 2xl:w-[1200px]">
+          <div className="flex flex-col sm:flex-row justify-between items-center sm:py-12">
+            <div className="flex flex-col justify-center sm:w-1/2">
+              <h1 className="font-bold text-3xl lg:text-4xl text-white mb-6 leading-tight">
+                The platform - where people are brought together
+              </h1>
+              <p className="text-lg text-white mb-8">
+                At <span className="font-bold">HAPNIN</span>, we believe that
+                local events and activities are the heart of any community.
+                That&apos;s why we created our app to help you discover the best
+                free and low cost events in your area.
+              </p>
+              <button className="bg-white text-purple-500 hover:text-white hover:bg-purple-500 rounded-full py-3 px-6 font-bold shadow-lg transition-all duration-200">
+                Get Started
+              </button>
+            </div>
+            <div className="w-full sm:w-1/2 mt-10 sm:mt-0">
+              <Image
+                src={barbecue}
+                alt="party"
+                className="w-full rounded-lg shadow-lg hover:scale-105 transition-all duration-500"
+              />
+            </div>
           </div>
-          <div className="w-full sm:w-1/2 mt-10 sm:mt-0">
-            <Image 
-              src={barbecue} 
-              alt="party"
-              className="w-full rounded-lg shadow-lg hover:scale-105 transition-all duration-500"
-            />
-          </div>
-        </div>
 
-        <ImageGrid   />
+          <ImageGrid />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
-
 
 const ImageGrid = () => {
   return (
@@ -85,6 +92,4 @@ const ImageGrid = () => {
   );
 };
 
-
-
-            export default Hero;
+export default Hero;
--- components/HowItWorks.tsx
+++ components/HowItWorks.tsx
@@ -1,36 +1,40 @@
-import Image, { StaticImageData } from "next/image";
 import React from "react";
+import { Element } from "react-scroll";
+import Image, { StaticImageData } from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
-import { Element } from "react-scroll";
 
 interface Props {
   name: string;
   description: string;
- // image: StaticImageData;
+  // image: StaticImageData;
 }
 
 function Card({ name, description }: Props) {
   return (
-    <div className="cursor-pointer rounded-md overflow-hidden shadow-md mb-5 min-w-lg h-48 flex bg-white hover:bg-green-50 transition-colors duration-300">
-      <div className="relative w-1/3 sm:w-1/4 h-full overflow-hidden">
-        <div className="absolute inset-0 bg-green-50 transform -rotate-6 origin-top-left"></div>
-        <div className="absolute inset-0 bg-green-50 transform rotate-6 origin-top-right"></div>
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="w-2/3 bg-green-200 h-px animate-pulse"></div>
+    <>
+      <Element name="howItWorks" />
+      <div className="cursor-pointer rounded-md overflow-hidden shadow-md mb-5 min-w-lg h-48 flex bg-white hover:bg-green-50 transition-colors duration-300">
+        <div className="relative w-1/3 sm:w-1/4 h-full overflow-hidden">
+          <div className="absolute inset-0 bg-green-50 transform -rotate-6 origin-top-left"></div>
+          <div className="absolute inset-0 bg-green-50 transform rotate-6 origin-top-right"></div>
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="w-2/3 bg-green-200 h-px animate-pulse"></div>
+          </div>
+        </div>
+        <div className="px-4 py-4 flex-shrink-0 w-2/3 sm:w-3/4 overflow-hidden">
+          <div className="font-medium mb-1 text-lg text-gray-800 hover:text-green-500 transition-colors duration-300">
+            {name || "Coffee"}
+          </div>
+          <p className="text-gray-700 text-sm overflow-hidden">{description}</p>
+          <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 mt-3 rounded-md transition-colors duration-300">
+            Learn More
+          </button>
         </div>
       </div>
-      <div className="px-4 py-4 flex-shrink-0 w-2/3 sm:w-3/4 overflow-hidden">
-        <div className="font-medium mb-1 text-lg text-gray-800 hover:text-green-500 transition-colors duration-300">{name || "Coffee"}</div>
-        <p className="text-gray-700 text-sm overflow-hidden">{description}</p>
-        <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 mt-3 rounded-md transition-colors duration-300">Learn More</button>
-      </div>
-    </div>
+    </>
   );
 }
-
-
-
 
 const HowItWorks = () => {
   const screen1 = require("../public/static/screen1.png");
@@ -40,7 +44,9 @@ const HowItWorks = () => {
     <div className="w-full px-6 sm:px-10 py-9">
       <div className="xl:mx-[6rem] 2xl:w-[1200px] 2xl:mx-auto">
         <div className="mb-4">
-          <h2 className="font-bold text-2xl text-center mb-2">How HAPNIN works</h2>
+          <h2 className="font-bold text-2xl text-center mb-2">
+            How HAPNIN works
+          </h2>
           <hr className="border-b-2 border-[#70f1ae] mx-auto w-20" />
         </div>
 
@@ -92,11 +98,11 @@ const HowItWorks = () => {
   );
 };
 
-
-
-const findEventDesc = "Looking for something to do in your local area? With HAPNIN, you can easily find events that match your interests and location. Whether you're into music, sports, or food, HAPNIN's user-friendly interface makes it easy to browse and discover new events that are happening near you. Simply search by category or location, and you'll be on your way to discovering new and exciting events in no time"
-const createEventDesc = "Looking to bring people together for a fun and exciting event? HAPNIN makes it easy to host events that are tailored to your interests and goals. Whether you're planning a birthday party, charity event, or conference, HAPNIN provides all the tools and resources you need to create an engaging and successful event. From event promotion to ticket sales, HAPNIN has you covered every step of the way."
-const attendEventDesc = "Love attending events, but hate the hassle of buying tickets and organizing logistics? With HAPNIN, attending events has never been easier. Our streamlined process lets you easily purchase tickets and coordinate with friends and family, so you can focus on enjoying the event itself. Plus, with helpful features like event reminders and in-app messaging, you'll always stay in the loop and be able to make the most of your event experience."
-
+const findEventDesc =
+  "Looking for something to do in your local area? With HAPNIN, you can easily find events that match your interests and location. Whether you're into music, sports, or food, HAPNIN's user-friendly interface makes it easy to browse and discover new events that are happening near you. Simply search by category or location, and you'll be on your way to discovering new and exciting events in no time";
+const createEventDesc =
+  "Looking to bring people together for a fun and exciting event? HAPNIN makes it easy to host events that are tailored to your interests and goals. Whether you're planning a birthday party, charity event, or conference, HAPNIN provides all the tools and resources you need to create an engaging and successful event. From event promotion to ticket sales, HAPNIN has you covered every step of the way.";
+const attendEventDesc =
+  "Love attending events, but hate the hassle of buying tickets and organizing logistics? With HAPNIN, attending events has never been easier. Our streamlined process lets you easily purchase tickets and coordinate with friends and family, so you can focus on enjoying the event itself. Plus, with helpful features like event reminders and in-app messaging, you'll always stay in the loop and be able to make the most of your event experience.";
 
 export default HowItWorks;
