4a1dc8d580496638dc03d2909e71ceb52ecf9a36
Henry Peters <hpeters@metawin.inc>
2023-05-06 20:12:03 +0100

    fix build err

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -13,9 +13,10 @@ const Hero = () => {
         <div className="mx-auto sm:mx-4 md:mx-12 lg:mx-0 lg:w-3/4 2xl:w-[1200px]">
           <div className="flex flex-col sm:flex-row justify-between items-center sm:pt-12 sm:pb-4 md:mb-6 lg:mb-8 2xl:pb-12">
             <div className="flex flex-col justify-center sm:w-1/2 sm:p-2">
-              <h1 className="font-bold text-3xl lg:text-4xl text-black mb-6 leading-tight">
-               The app that helps you find out what’s HAPNIN in your area!
-              </h1>
+            <h1 className="font-bold text-3xl lg:text-4xl text-black mb-6 leading-tight">
+              The app that helps you find out what's HAPNIN in your area!
+                </h1>
+
               <p className="text-lg text-black mb-8">
                  <span className="font-bold">HAPNIN</span> is an event discovery app that offers user-generated events and verified partner events in your area. The app's easy-to-use map-based interface makes it simple to find things to do and exclusive discounts. Download HAPNIN now to plan your next night out with friends and create memories that last a lifetime!
               </p>
